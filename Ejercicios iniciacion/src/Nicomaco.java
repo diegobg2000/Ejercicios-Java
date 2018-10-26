377f53e7a70b8672212daeefd39bcd9addb366b5
Diego <bascuasinformatica2015@gmail.com>
2018-10-26 10:35:08 +0200

    Actualizacion

--- Ejercicios iniciacion/src/Nicomaco.java
+++ Ejercicios iniciacion/src/Nicomaco.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
 
 public class Nicomaco {
+		public static void main(String[] args) {
+			Scanner in = new Scanner (System.in);
+			System.out.println("Escribe la cantidad de numeros cubos: ");
+			int n = in.nextInt();
+			int sgteImpar = 1;
+			
+			for (int i = 1; i <= n; i++ ) {
+				int acumulador = 0;
+				System.out.print(i + "^3 =  ");
+						for (int j=1; j<=i; j++) {
+							if (j != 1) {
+								System.out.print(sgteImpar + " + ");
+							}
+							acumulador += sgteImpar;
+							sgteImpar += 2;
+							
+						}			
+				System.out.println( " = " + acumulador);
+			
+			}
+		}
+	}
 
-}
+
